425767a4d0ded9a40d64676f7e72d9c7e9667e69
Peter Wolf <pwolf2310@gmail.com>
2021-02-24 00:18:58 +0100

    clean up while loop in Split

--- lib/gamelog/src/gamelog.cpp
+++ lib/gamelog/src/gamelog.cpp
@@ -49,33 +49,27 @@ std::vector<std::string> Split(std::string const& content)
     std::vector<std::string> tokens;
     char const* sentinel = "@P";
     size_t const sentinel_length{2};
-    size_t const len{content.length()};
-    size_t pos{0};
-    while ((pos < len) && (pos != std::string::npos))
+    size_t pos{content.find(sentinel)};
+    while (pos != std::string::npos)
     {
-        pos = content.find(sentinel, pos);
-        if (pos != std::string::npos)
+        std::string token;
+        size_t const next_pos{content.find(sentinel, pos + sentinel_length)};
+        if ((next_pos != std::string::npos) && ((pos + sentinel_length) < next_pos))
         {
-            std::string token;
-            size_t const next_pos{content.find(sentinel, pos + sentinel_length)};
-            if ((next_pos != std::string::npos) && ((pos + sentinel_length) < next_pos))
-            {
-                token =
-                    content.substr(pos + sentinel_length, (next_pos - pos - sentinel_length + 1));
-            }
-            else if (next_pos == std::string::npos)
-            {
-                token = content.substr(pos + sentinel_length);
-            }
-
-            if (token.length() > 0)
-            {
-                CutOff(token);
-                tokens.push_back(token);
-            }
-
-            pos = next_pos;
+            token = content.substr(pos + sentinel_length, (next_pos - pos - sentinel_length + 1));
         }
+        else if (next_pos == std::string::npos)
+        {
+            token = content.substr(pos + sentinel_length);
+        }
+
+        if (token.length() > 0)
+        {
+            CutOff(token);
+            tokens.push_back(token);
+        }
+
+        pos = next_pos;
     }
     return tokens;
 }
