466c5d2e666af9205b966c4d7c2415f3342c5f91
Peter Wolf <pwolf2310@gmail.com>
2021-03-02 00:01:13 +0100

    implement writing results to file and supporting the CL args

--- src/file_parser/src/file_parser.cpp
+++ src/file_parser/src/file_parser.cpp
@@ -1,5 +1,8 @@
 #include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <string>
+#include <system_error>
 
 #include "gamelog/gamelog.h"
 
@@ -17,25 +20,32 @@ static void show_usage(std::string const& exec_name)
         << "\t\t\t\tThe resulting file name(s) will be Match_GameLog_$ID.log with the $ID of the "
         << "parsed match.\n"
         << "\t-t, --target TARGET_DIR\tIf set, output file(s) will be written into this "
-        << "directory.\n"
-        << "\t\t\t\tIf TARGET_DIR does not exist, it will be created.\n";
+        << "directory (default: current working directory).\n"
+        << "\t\t\t\tIf TARGET_DIR does not exist, this tool will try to create it.\n";
 }
 
 
 static void ParseFile(std::filesystem::path const& to_parse, std::filesystem::path const& output)
 {
+    std::cout << "Parsing " << to_parse.filename() << ":\n";
     auto const lines = gl::ParseGameLogFile(to_parse);
-    if (output.empty())
+
+    std::ofstream out{output};
+    for (auto const& line : lines)
     {
-        std::cout << "Parsing " << to_parse.filename() << ":\n";
-        for (auto const& line : lines)
-        {
-            std::cout << line << "\n";
-        }
+        out << line << "\n";
     }
-    else
+    std::cout << "Results written to " << output << "\n";
+}
+
+
+static void ParseFile(std::filesystem::path const& to_parse)
+{
+    std::cout << "Parsing " << to_parse.filename() << ":\n";
+    auto const lines = gl::ParseGameLogFile(to_parse);
+    for (auto const& line : lines)
     {
-        // TODO: write output to file
+        std::cout << line << "\n";
     }
 }
 
@@ -43,7 +53,21 @@ static void ParseFile(std::filesystem::path const& to_parse, std::filesystem::pa
 static bool IsMatchGameLog(std::filesystem::path const& file_path)
 {
     return (file_path.extension() == ".dat") &&
-           (file_path.filename().generic_string().find("Match_GameLog_") == 0);
+           (file_path.filename().string().find("Match_GameLog_") == 0);
+}
+
+
+static bool CreateDirectories(std::filesystem::path const& dir_path)
+{
+    std::error_code dir_error;
+    std::filesystem::create_directories(dir_path, dir_error);
+    if (dir_error.value() != 0)
+    {
+        std::cerr << "Error: Could not create directories: " << dir_path << ": "
+                  << dir_error.message() << "\n";
+        return false;
+    }
+    return true;
 }
 
 
@@ -55,45 +79,69 @@ int main(int argc, char* const* argv)
         return 1;
     }
 
-    std::filesystem::path file_name;
-    std::string output;
+    std::filesystem::path file_name{};
+    std::filesystem::path target{std::filesystem::current_path()};
+    bool output_to_file{false};
 
     // Parse command line arguments
-    for (int i = 1; i < argc; ++i)
+    for (int i{1}; i < argc; i++)
     {
-        std::string param = argv[i]; // NOLINT
+        std::string param{argv[i]}; // NOLINT
         if ((param == "-h") || (param == "--help"))
         {
             show_usage(argv[0]); // NOLINT
             return 0;
         }
-        if ((param == "-o") || (param == "--output-prefix"))
+        if ((param == "-o") || (param == "--output"))
         {
-            if (i + 1 < argc)
+            output_to_file = true;
+        }
+        else if ((param == "-t") || (param == "--target"))
+        {
+            if ((i + 1) < argc)
             {
-                output = argv[i++]; // NOLINT
+                target = std::filesystem::path{argv[++i]}; // NOLINT
             }
             else
             {
-                std::cerr << "Error: --output option requires one argument.\n";
+                std::cerr << "Error: --target option requires one argument.\n";
                 show_usage(argv[0]); // NOLINT
                 return 1;
             }
         }
         else
         {
-            file_name = argv[i]; // NOLINT
+            file_name = std::filesystem::path{argv[i]}; // NOLINT
         }
     }
 
+    target /= "placeholder.dat"; // setup target for replacement ooperations
+
     if (std::filesystem::is_directory(file_name))
     {
+        if (output_to_file)
+        {
+            // Create directories for output files.
+            if (!CreateDirectories(target.parent_path()))
+            {
+                return 1;
+            }
+        }
+
         // Iterate through directory and parse all match game logs.
         for (auto const& file : std::filesystem::recursive_directory_iterator{file_name})
         {
             if ((std::filesystem::is_regular_file(file)) && (IsMatchGameLog(file)))
             {
-                ParseFile(file, output);
+                if (output_to_file)
+                {
+                    target.replace_filename(file.path().filename()).replace_extension(".log");
+                    ParseFile(file, target);
+                }
+                else
+                {
+                    ParseFile(file);
+                }
             }
         }
     }
@@ -105,7 +153,20 @@ int main(int argc, char* const* argv)
             std::cerr << "Error: " << file_name << " is (likely) no MTGO match game log.\n";
             return 1;
         }
-        ParseFile(file_name, std::filesystem::path{output});
+        if (output_to_file)
+        {
+            // Create directories for output file.
+            if (!CreateDirectories(target.parent_path()))
+            {
+                return 1;
+            }
+            ParseFile(file_name,
+                      target.replace_filename(file_name.filename()).replace_extension(".log"));
+        }
+        else
+        {
+            ParseFile(file_name);
+        }
     }
     else
     {
@@ -113,5 +174,6 @@ int main(int argc, char* const* argv)
                   << " is neither a directory nor a regular file.\n";
         return 1;
     }
+
     return 0;
 }
